9757ee1b8fc9286000ba073be898f8642f2b1443
Clinton Fillier <clinton@fillier.ca>
2024-02-18 23:43:14 -0330

    Changed to use RCON commands instead of child processes.

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ WORKDIR /usr/src/bot
 COPY package.json /usr/src/bot
 RUN npm install
 
-COPY . /usr/src/bot
+COPY src /usr/src/bot/src
 
 # Start the bot.
 CMD ["node", "src/index.js"]
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 // Import the necessary classes from discord.js library and the dotenv package
 import { Client, GatewayIntentBits, Partials } from 'discord.js';
 import dotenv from 'dotenv';
-import process from 'process';
-import { spawn } from 'child_process';
-
-process.on('SIGINT', () => {
-  console.info("Interrupted")
-  process.exit(0)
-})
+import Rcon from 'rcon';
 
 // Configure dotenv to load the environment variables from the .env file
 dotenv.config();
@@ -40,57 +34,52 @@ client.on("messageCreate", message => {
   const command = args.shift().toLowerCase();
 
   if (command === "help") {
+    console.log("command: help");
     message.reply("Available commands are: ping, online, restart");
   }
 
   if (command === "ping") {
+    console.log("command: ping");
     const timeTaken = Date.now() - message.createdTimestamp;
     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
   }
 
   if (command === "online") {
-    getPlayers(message)
+    console.log("command: online");
+    rcon("players", message);
   }
 
   if (command === "restart") {
-    restartServer()
-    message.reply(`Server restarted...this will take some time.`);
+    console.log("command: restart");
+    rcon("quit", null);
+    message.reply(`Restarting server...this will take some time.`);
   }
 });
 
-function getPlayers(message) {
-  var response = '';
-  const child = spawn('rconclt', ['zomboid', 'players']);
-
-  child.stdout.on("data", (data) => {
-    console.log(`stdout: ${data}`);
-    response += data;
+function rcon(command, message) {
+  const conn = new Rcon(process.env.RCON_HOST, process.env.RCON_PORT, process.env.RCON_PASSWORD);
+  conn.on('auth', () => {
+    // You must wait until this event is fired before sending any commands,
+    // otherwise those commands will fail.
+    console.log("Authenticated");
+    console.log(`Sending RCON command: ${command}`);
+    conn.send(command);
+  });
+  conn.on('response', (str) => {
+    if(str.length > 0) {
+      console.log("Response: " + str);
+      if(message) message.reply(str);
+      conn.disconnect();
+    }
+  });
+  conn.on('error', (err) => {
+    console.log("Error: " + err);
+  });
+  conn.on('end', () => {
+    console.log("Connection closed");
   });
 
-  child.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-
-  child.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
-    message.reply(response);
-  });
-}
-
-function restartServer() {
-  const child = spawn('docker', ['restart', 'zomboid-server']);
-
-  child.stdout.on("data", (data) => {
-    console.log(`stdout: ${data}`);
-  });
-
-  child.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-
-  child.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
+  conn.connect();
 }
 
 // Log in the bot with the token from the environment variables and handle potential login errors
